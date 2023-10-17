b9504993e4bfce0f4e98252adb54f1f552a3e35a
Alexandros Dridakis <alexandros.dridakis@retailzoom.net>
2023-10-17 13:17:38 +0300

    added host directive to list item

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,37 +1,51 @@
 import { Component } from '@angular/core';
-import {NgFor} from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import {
   CdkDragDrop,
   moveItemInArray,
   transferArrayItem,
   CdkDrag,
   CdkDropList,
+  CdkDragStart,
 } from '@angular/cdk/drag-drop';
-import { ListItemComponent } from "../list-item/list-item.component";
-
+import { ListItemComponent } from '../list-item/list-item.component';
 
 @Component({
-    selector: 'app-board',
-    standalone: true,
-    templateUrl: './board.component.html',
-    styleUrls: ['./board.component.scss'],
-    imports: [CdkDropList, NgFor, CdkDrag, ListItemComponent]
+  selector: 'app-board',
+  standalone: true,
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss'],
+  imports: [CdkDropList, NgFor, NgIf, ListItemComponent],
 })
 export class BoardComponent {
   todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
   done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
+  isDragging = false;
+
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
     }
+  }
+
+  onDragStart() {
+    this.isDragging = true;
+  }
+
+  onDragEnd() {
+    this.isDragging = false;
   }
 }
--- src/app/components/list-item/list-item.component.ts
+++ src/app/components/list-item/list-item.component.ts
@@ -1,17 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CdkDrag } from '@angular/cdk/drag-drop';
+import { CdkDrag, DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-list-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DragDropModule],
   templateUrl: './list-item.component.html',
   styleUrls: ['./list-item.component.scss'],
   hostDirectives: [
-    CdkDrag
-  ]
+    {
+      directive: CdkDrag,
+      outputs: ['cdkDragStarted: dragStart', 'cdkDragEnded: dragEnd'],
+    },
+  ],
 })
 export class ListItemComponent {
   @Input() text!: string;
+
+  dragStart() {
+    console.log('start drag');
+  }
 }
