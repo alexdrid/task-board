b96c1e4abf41e1fcd506bbc000ffb537b1c8c7e6
Alexandros Dridakis <alexandros.dridakis@retailzoom.net>
2023-10-17 12:39:55 +0300

    created board

---
+++ src/app/components/board/board.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {NgFor} from '@angular/common';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
+
+
+@Component({
+  selector: 'app-board',
+  standalone: true,
+  imports: [CdkDropList, NgFor, CdkDrag],
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss'],
+})
+export class BoardComponent {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+}
